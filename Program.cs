f304355d80e1832a3bd352a039cf81a4e8e247ec
suraj8052442710 <97836818+suraj8052442710@users.noreply.github.com>
2023-03-28 21:30:48 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,38 +10,46 @@ namespace switch_calculator
     {
         static void Main(string[] args)
         {
-            double num1 = 0;
-            double num2 = 0;
-            double result = 0;
-
-            Console.WriteLine("enter num1:");
-            num1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("enter num2:");
-            num2 = Convert.ToDouble(Console.ReadLine());
 
 
-            Console.WriteLine("enter an option");
-            Console.WriteLine("+ : Add");
-            Console.WriteLine(" - : Substract");
-            Console.WriteLine(" * : Multiply");
-
-            switch (Console.ReadLine())
+            do
             {
-                case "+":
-                    result = num1 + num2;
-                    Console.WriteLine("your result is {num1} + {num2} :" + result);
-                    break;
-                case "-":
-                    result = num1 - num2;
-                    Console.WriteLine("your result is {num1} - {num2} :" + result);
-                    break;
-                case "*":
-                    result = num1 * num2;
-                    Console.WriteLine("your result is {num1} * {num2} :" + result);
-                    break;
+                double num1 = 0;
+                double num2 = 0;
+                double result = 0;
 
-            }
+                Console.WriteLine("/*enter num1:*/");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("enter num2:");
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+
+                Console.WriteLine("enter an option");
+                Console.WriteLine("+ : Add");
+                Console.WriteLine(" - : Substract");
+                Console.WriteLine(" * : Multiply");
+
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        result = num1 + num2;
+                        Console.WriteLine("your result is {num1} + {num2} :" + result);
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        Console.WriteLine("your result is {num1} - {num2} :" + result);
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        Console.WriteLine("your result is {num1} * {num2} :" + result);
+                        break;
+
+                }
+                Console.WriteLine("would you like to continue (esc = yes, alt = no):");
+            } while (Console.ReadLine().ToUpper() == "esc");
+
+            
             Console.ReadKey();
 
 
