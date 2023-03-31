047278ad79c7840cbfec602e931cc6eea2d49240
suraj8052442710 <97836818+suraj8052442710@users.noreply.github.com>
2023-03-31 11:20:47 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,43 +4,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp9
+namespace switch_calculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+               
+
+            do
+            {
+                double num1 = 0;
+                double num2 = 0;
+                double result = 0;
+
+                Console.WriteLine("/*enter num1:*/");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("enter num2:");
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+
+                Console.WriteLine("enter an option");
+                Console.WriteLine("+ : Add");
+                Console.WriteLine(" - : Substract");
+                Console.WriteLine(" * : Multiply");
+                
+
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        result = num1 + num2;
+                        Console.WriteLine("your result is {num1} + {num2} :" + result);
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        Console.WriteLine("your result is {num1} - {num2} :" + result);
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        Console.WriteLine("your result is {num1} * {num2} :" + result);
+                        break;
+
+                }
+                Console.WriteLine("would you like to continue (esc = yes, alt = no):");
+            } while (Console.ReadLine().ToUpper() == "esc");
 
             
-            
-            Console.WriteLine("Hello Sir I am Microsoft Company Bot");
-            Console.WriteLine("For Register in our company please write your name and press enter ");
-            Console.Write("");
-            Console.ReadLine();
-
-            Console.WriteLine("Please write your age and press enter");
-            Console.Write("");
-            Console.ReadLine();
-
-            Console.WriteLine("Write your gmail id carefully and press enter");
-            Console.Write("");
-            Console.ReadLine();
-
-            Console.WriteLine("Enter Your mobile number: press Enter ");
-            Console.Write("");
-            Console.ReadLine();
-
-            Console.WriteLine("You are successfully registered in our company");
-            Console.WriteLine("For any enquiry contact our Microsoft customer care number");
-            Console.WriteLine("1800 102 1100 ");
-            Console.WriteLine("THANK YOU");
-            Console.ReadLine();
+            Console.ReadKey();
 
 
 
 
 
 
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
     }
 }
